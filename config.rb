3c0b1fbb973c7925ab954fd311f1779ab5d1a667
Eric Gardner <gardner.ec@gmail.com>
2020-12-20 10:25:08 -0800

    Initial commit
    In the beginning there was darkness

---
+++ config.rb
@@ -0,0 +1,45 @@
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+
+# activate :autoprefixer do |prefix|
+#   prefix.browsers = "last 2 versions"
+# end
+
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page '/path/to/file.html', layout: 'other_layout'
+
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
+
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
+# Helper Methods
+
+helpers do
+  # Expects dates in 2020-12-23 format
+  def format_date date
+    return Date.parse( date ).strftime("%B %d, %Y")
+  end
+end
+
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
